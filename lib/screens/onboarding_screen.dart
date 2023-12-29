008a60dd6f9d14310acd210bf4eedfb7b8377fb4
Satyam Mishra <satyam12295@gmail.coma>
2023-12-29 11:10:19 +0530

    commits

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -5,6 +5,8 @@ class Onboarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
